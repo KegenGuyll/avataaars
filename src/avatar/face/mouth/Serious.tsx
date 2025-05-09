2c838fbf70b946705ac39348d865cde08ed6dfd5
Kegen Guyll <guyllkegen@gmail.com>
2025-05-08 21:20:12 -0500

    Refactor mouth components to functional components with ExtendedComponent type
    - Converted Disbelief, Eating, Grimace, Sad, ScreamOpen, Serious, Smile, Tongue, Twinkle, and Vomit classes to functional components.
    - Added optionValue static property to each component.
    - Removed lodash uniqueId usage in favor of React's useId for generating unique IDs.
    - Updated component structure for improved readability and performance.

--- src/avatar/face/mouth/Serious.tsx
+++ src/avatar/face/mouth/Serious.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react'
+import { ExtendedComponent } from '../../../models/ExtendedComponent'
 
-export default class Serious extends React.Component {
-  static optionValue = 'Serious'
-
-  render () {
-    return (
-      <g
-        id='Mouth/Serious'
-        transform='translate(2.000000, 52.000000)'
-        fill='#000000'
-        fillOpacity='0.699999988'>
-        <rect id='Why-so-serious?' x='42' y='18' width='24' height='6' rx='3' />
-      </g>
-    )
-  }
+const Serious: ExtendedComponent = () => {
+  return (
+    <g
+      id='Mouth/Serious'
+      transform='translate(2.000000, 52.000000)'
+      fill='#000000'
+      fillOpacity='0.699999988'>
+      <rect id='Why-so-serious?' x='42' y='18' width='24' height='6' rx='3' />
+    </g>
+  )
 }
+
+Serious.optionValue = 'Serious'
+
+export default Serious
