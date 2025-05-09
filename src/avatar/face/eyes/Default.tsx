7d4df9f1aefa608a6c339f849ece9d4330e1e7d9
Kegen Guyll <guyllkegen@gmail.com>
2025-05-08 20:40:14 -0500

    Refactor eye components to functional components and add optionValue property
    - Converted class components for Cry, Default, Dizzy, EyeRoll, Happy, Hearts, Side, Surprised, Wink, and WinkWacky to functional components.
    - Introduced ExtendedComponent type for better type safety.
    - Added optionValue static property to each component for consistent identification.
    - Cleaned up imports and improved readability of the code.

--- src/avatar/face/eyes/Default.tsx
+++ src/avatar/face/eyes/Default.tsx
@@ -1,17 +1,18 @@
-import * as React from 'react'
+import React from 'react'
+import { ExtendedComponent } from '../../../models/ExtendedComponent'
 
-export default class Default extends React.Component {
-  static optionValue = 'Default'
-
-  render () {
-    return (
-      <g
-        id='Eyes/Default-😀'
-        transform='translate(0.000000, 8.000000)'
-        fillOpacity='0.599999964'>
-        <circle id='Eye' cx='30' cy='22' r='6' />
-        <circle id='Eye' cx='82' cy='22' r='6' />
-      </g>
-    )
-  }
+const Default: ExtendedComponent = () => {
+  return (
+    <g
+      id='Eyes/Default-😀'
+      transform='translate(0.000000, 8.000000)'
+      fillOpacity='0.599999964'>
+      <circle id='Eye' cx='30' cy='22' r='6' />
+      <circle id='Eye' cx='82' cy='22' r='6' />
+    </g>
+  )
 }
+
+Default.optionValue = 'Default'
+
+export default Default
