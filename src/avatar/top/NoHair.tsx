8753904f88667a1c30a18a9732a1ab91438f174b
Kegen Guyll <guyllkegen@gmail.com>
2025-05-11 17:51:49 -0500

    Refactor WinterHat components to use functional components and hooks
    - Converted WinterHat2, WinterHat3, and WinterHat4 from class components to functional components.
    - Replaced uniqueId from lodash with useId from React for generating unique IDs.
    - Updated props handling to use TypeScript interfaces for better type safety.
    - Improved code readability and structure by removing unnecessary class methods and properties.
    - Ensured consistent usage of ReactNode for children props across all components.

--- src/avatar/top/NoHair.tsx
+++ src/avatar/top/NoHair.tsx
@@ -1,57 +1,59 @@
-import * as React from 'react'
-import { uniqueId } from 'lodash'
-
+import React, { useId, ReactNode } from 'react'
 import FacialHair from './facialHair'
+import { ExtendedComponent } from '../../models/ExtendedComponent'
 
-export default class NoHair extends React.Component {
-  static optionValue = 'NoHair'
+interface NoHairProps {
+  children?: ReactNode
+}
 
-  private filter1 = uniqueId('react-filter-')
-  private mask1 = uniqueId('react-mask-')
-  private path1 = uniqueId('react-path-')
+const NoHair: ExtendedComponent<NoHairProps> = ({ children }: NoHairProps) => {
+  const filter1 = useId()
+  const mask1 = useId()
+  const path1 = useId()
 
-  render () {
-    const { filter1, mask1, path1 } = this
-    return (
-      <g id='Top' strokeWidth='1' fillRule='evenodd'>
-        <defs>
-          <rect id={path1} x='0' y='0' width='264' height='280' />
-          <filter
-            x='-0.8%'
-            y='-2.0%'
-            width='101.5%'
-            height='108.0%'
-            filterUnits='objectBoundingBox'
-            id={filter1}>
-            <feOffset
-              dx='0'
-              dy='2'
-              in='SourceAlpha'
-              result='shadowOffsetOuter1'
-            />
-            <feColorMatrix
-              values='0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.16 0'
-              type='matrix'
-              in='shadowOffsetOuter1'
-              result='shadowMatrixOuter1'
-            />
-            <feMerge>
-              <feMergeNode in='shadowMatrixOuter1' />
-              <feMergeNode in='SourceGraphic' />
-            </feMerge>
-          </filter>
-        </defs>
-        <mask id={mask1} fill='white'>
-          <use xlinkHref={'#' + path1} />
-        </mask>
-        <g id='Mask' />
-        <g id='Top/No-Hair' mask={`url(#${mask1})`}>
-          <g transform='translate(-1.000000, 0.000000)'>
-            <FacialHair />
-            {this.props.children}
-          </g>
+  return (
+    <g id='Top' strokeWidth='1' fillRule='evenodd'>
+      <defs>
+        <rect id={path1} x='0' y='0' width='264' height='280' />
+        <filter
+          x='-0.8%'
+          y='-2.0%'
+          width='101.5%'
+          height='108.0%'
+          filterUnits='objectBoundingBox'
+          id={filter1}>
+          <feOffset
+            dx='0'
+            dy='2'
+            in='SourceAlpha'
+            result='shadowOffsetOuter1'
+          />
+          <feColorMatrix
+            values='0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.16 0'
+            type='matrix'
+            in='shadowOffsetOuter1'
+            result='shadowMatrixOuter1'
+          />
+          <feMerge>
+            <feMergeNode in='shadowMatrixOuter1' />
+            <feMergeNode in='SourceGraphic' />
+          </feMerge>
+        </filter>
+      </defs>
+      <mask id={mask1} fill='white'>
+        <use xlinkHref={`#${path1}`} />
+      </mask>
+      <g id='Mask' />
+      <g id='Top/No-Hair' mask={`url(#${mask1})`}>
+        <g transform='translate(-1.000000, 0.000000)'>
+          <FacialHair />
+          {children}
         </g>
       </g>
-    )
-  }
+    </g>
+  )
 }
+
+NoHair.optionValue = 'NoHair'
+
+export default NoHair
