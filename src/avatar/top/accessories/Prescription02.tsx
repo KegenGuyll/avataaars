7b0d086ff561c9a18a9534d7863d36cf65428886
Kegen Guyll <guyllkegen@gmail.com>
2025-05-11 17:06:16 -0500

    refactor: remove unused uniqueId import from multiple components

--- src/avatar/top/accessories/Prescription02.tsx
+++ src/avatar/top/accessories/Prescription02.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { uniqueId } from 'lodash'
 import { ExtendedComponent } from '../../../models/ExtendedComponent'
 
 const Prescription02: ExtendedComponent = () => {
--- src/avatar/top/facialHair/BeardMajestic.tsx
+++ src/avatar/top/facialHair/BeardMajestic.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { uniqueId } from 'lodash'
 import { ExtendedComponent } from '../../../models/ExtendedComponent'
 
 import Colors from './Colors'
